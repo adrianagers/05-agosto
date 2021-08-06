4328e42f8fff5a552a555e5f66a9689f7fba89ea
Adriana Geraldine Romero Sierra <53448861+adrianagers@users.noreply.github.com>
2021-08-05 19:39:00 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>geral</title>
+</head>
+<body>
+    <h1>rapido geral</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum esse reprehenderit, nostrum enim doloribus exercitationem ullam nesciunt quisquam culpa harum commodi et? At rerum id ex, dolores illum modi, ea ullam, quo vel sequi quidem cupiditate libero quam! Reprehenderit explicabo quia beatae dolores perferendis! Enim aliquid suscipit illum iste beatae, doloribus possimus sequi. Expedita explicabo, sapiente eligendi doloremque alias facere praesentium eum ipsum illo quo aspernatur iure consectetur quis ut a porro perspiciatis velit placeat cupiditate voluptate hic, laudantium nihil minus? Incidunt veritatis corrupti doloremque quo enim dignissimos accusantium laudantium, illum explicabo unde quisquam tempora maxime omnis est magni saepe consectetur numquam. Earum, assumenda nam.</p>
+</body>
+</html>
